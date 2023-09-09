1e05f8f57ccde1654fb99990191acb356cd0e714
Vinicius Mota <vinicius.motamorici@gmail.com>
2023-09-09 18:57:07 -0300

    Clean Files and footer component

---
+++ src/components/footer/intex.tsx
@@ -0,0 +1,11 @@
+export function Footer(){
+  
+  return(
+    <footer>
+      <section></section>
+      <section>
+        <img src="../../assets/tmdb_logo.svg" alt="" />
+      </section>
+    </footer>
+  )
+}
